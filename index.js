da09dd4b9a28b66013842a8f4bbda7eb83c062f3
Levi Bohn <levibohn@gmail.com>
2022-10-19 05:17:06 -0500

    successfully completed task 3.

--- index.js
+++ index.js
@@ -94,10 +94,19 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(scoreBoard, whatInning){
-  /*Code Here*/
+function finalScore(inningCB, inningsPlayed){
+  let homeScore = 0;
+  let awayScore = 0;
+  for (let i=0; i<inningsPlayed; i++) {
+    homeScore = homeScore + inningCB();
+    awayScore = awayScore + inningCB();
+  }
+  return {
+    Home: homeScore,
+    Away: awayScore
+  }
 }
-
+console.log ('task 3:', finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
